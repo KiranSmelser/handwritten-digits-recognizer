e68fb8b17c3d29a9bb15d1d46b586da8e5f60e99
Kiran <kiran.smelser@gmail.com>
2022-03-23 10:21:22 -0700

    Tested model with five-test.png

--- image_to_bytes.py
+++ image_to_bytes.py
@@ -6,10 +6,10 @@ from PIL import Image
 def main():
     img = Image.open("five-test.png")
     raw_data = list(img.getdata())
+    print(raw_data)
     # Format image data.
     formatted_data = []
     for i in range(len(raw_data)):
-        for j in range(len(raw_data[i])):
-            formatted_data.append(255 - raw_data[i][j])
+        formatted_data.append(255 - raw_data[i][0])
     print(formatted_data)
 main()
