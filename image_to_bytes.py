76898a7ce1790dab9a9909b41093fe499e57d5d4
Kiran <kiran.smelser@gmail.com>
2022-03-23 10:04:23 -0700

    Converted image data to bytes

--- image_to_bytes.py
+++ image_to_bytes.py
@@ -4,5 +4,12 @@
 from PIL import Image
 
 def main():
-
+    img = Image.open("five-test.png")
+    raw_data = list(img.getdata())
+    # Format image data.
+    formatted_data = []
+    for i in range(len(raw_data)):
+        for j in range(len(raw_data[i])):
+            formatted_data.append(255 - raw_data[i][j])
+    print(formatted_data)
 main()
